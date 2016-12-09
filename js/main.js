6b89449af6c53c53baa37c01a979321eff1f7ecc
Herop <thesecon@gmail.com>
2016-12-09 19:26:08 +0900

    where-is 섹션 추가
    pricing-card 섹션 추가
      - display: table(table-cell) 활용 중...

--- js/main.js
+++ js/main.js
@@ -1,1 +1,18 @@
 'use strict';
+
+function initMap() {
+    var myLatLng = {
+        lat: 37.782293,
+        lng: -122.391240
+    };
+    var map = new google.maps.Map(document.getElementById('map'), {
+        center: myLatLng,
+        scrollwheel: false,
+        zoom: 17
+    });
+    var marker = new google.maps.Marker({
+        position: myLatLng,
+        map: map,
+        title: 'gitHub.com'
+    });
+}
