c40ef27a59a16620e9f168094a40380c573416a7
Herop <thesecon@gmail.com>
2016-12-13 18:59:11 +0900

    @media 마무리 작업

--- js/main.js
+++ js/main.js
@@ -1,18 +1,37 @@
 'use strict';
 
 function initMap() {
+
     var myLatLng = {
         lat: 37.782293,
         lng: -122.391240
     };
+
     var map = new google.maps.Map(document.getElementById('map'), {
         center: myLatLng,
         scrollwheel: false,
-        zoom: 17
+        zoom: 18
     });
+
     var marker = new google.maps.Marker({
         position: myLatLng,
         map: map,
-        title: 'gitHub.com'
+        title: 'GitHub'
     });
+
 }
+
+$(function () {
+
+    var $toggle = $('.toggle');
+    var $toggleBtn = $('#toggle-btn');
+
+    $toggleBtn.on('click', function () {
+            $toggle.toggleClass('on');
+    });
+
+    $(window).on('resize', function () {
+        if ($(this).width() > 1020) $toggle.removeClass('on');
+    });
+
+});
