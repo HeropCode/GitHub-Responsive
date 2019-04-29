503f285641165835e227cc269f0d916253b0bcfa
ParkYoungWoong <thesecon@gmail.com>
2019-04-30 07:47:29 +0900

    Modify to new code
    새로운 코드로 변경 중..

--- js/main.js
+++ js/main.js
@@ -1,28 +1,30 @@
-$(function () {
+(function (window, document) {
   'use strict';
 
-  var $toggle = $('.toggle');
-  var $toggleBtn = $('#toggle-btn');
-  var $signInput = $('.sign-form input');
-
-  // '햄버거 버튼'을 클릭했을 경우
-  $toggleBtn.on('click', function () {
-    $toggle.toggleClass('on');
+  const $toggle = document.querySelectorAll('.toggle');
+  const $toggleBtn = document.querySelector('#toggle-btn');
+  const $signForm = document.querySelector('.sign-form');
+  const $signInput = $signForm.querySelector('input[type=text]');
+  const $signSubmit = $signForm.querySelector('input[type=submit]');
+  
+  $toggleBtn.addEventListener('click', function () {
+    $toggle.forEach(function (toggle) {
+      toggle.classList.toggle('on');
+    });
   });
 
-  // 브라우저의 사이즈가 변경될 때
-  // 사이즈가 1200px 이상 일 경우 '메뉴' 보임
-  $(window).on('resize', function () {
-    if ($(this).width() > 1200) $toggle.removeClass('on');
-  });
-
-  // `<input>` 에서 `tab`가 아닌 `enter`를 눌렀을 때
-  // 다음 `<input>`으로 넘어가도록 설정
-  $signInput.on('keydown', function (event) {
-    if (event.which === 13) {
-      event.preventDefault();
-      $(event.target).parent().next().find('input, button').focus();
+  window.addEventListener('resize', function () {
+    if (window.innerWidth > 1200) {
+      $toggle.forEach(toggle => {
+        toggle.classList.remove('on');
+      });
     }
   });
 
-});
+  $signInput.addEventListener('keydown', function (event) {
+    if (event.which === 13) { // ENTER key
+      event.preventDefault();
+      $signSubmit.focus();
+    }
+  })
+})(window, document)
